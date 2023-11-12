3749860b5592b293ab17da638148e5214c354cf8
Mark Patka <Mark.Kaz.1998@gmail.com>
2023-11-12 17:49:00 +0300

    Initialize the Project

--- SystemExplorer/ViewModels - Copy/MainViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SystemExplorer.ViewModels;
-
-public class MainViewModel : ViewModelBase
-{
-    public string Greeting => "Welcome to Avalonia!";
-}
--- SystemExplorer/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace SystemExplorer.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
